c730002ea328f595442787b4fdb425e7a6f89ecb
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-11-25 11:45:17 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -267,4 +267,3 @@ async function getAPIData() {
     const response = await axios.get('https://jsonplaceholder.typicode.com/albums');
     return response.data[0].title;
 }
-module.exports = TestAPICallIntentHandler;
